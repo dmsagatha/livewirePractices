74055611b3dee161b7b020447421b7234cd40171
Doris Morales <dmorales55@gmail.com>
2021-01-07 21:46:04 -0500

    Orders - Migrations

---
+++ database/migrations/2021_01_07_212811_create_order_product_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductTable extends Migration
+{
+  public function up()
+  {
+    Schema::create('order_product', function (Blueprint $table) {
+      $table->foreignId('order_id')->constrained();
+      $table->foreignId('product_id')->constrained();
+      $table->integer('quantity');
+    });
+  }
+
+  public function down()
+  {
+    Schema::dropIfExists('order_product');
+  }
+}
