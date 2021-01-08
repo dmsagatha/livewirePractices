f73d84d0b082a5feb76909d133b08cd9904e3566
Doris Morales <dmorales55@gmail.com>
2021-01-07 21:49:58 -0500

    Orders-Products - Relationships

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
   use HasFactory;
+
+  protected $fillable = ['customer_name', 'customer_email'];
+
+  public function products()
+  {
+    return $this->belongsToMany(Product::class);
+  }
 }
