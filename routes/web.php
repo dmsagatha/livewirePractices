d8169c249c92a55fd9869c31cd4fa45fc8b8a4dc
Doris Morales <dmorales55@gmail.com>
2021-01-15 09:22:33 -0500

    Barra de depuración de Telescope

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\OrderController;
 use App\Http\Livewire\Users;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
   return view('welcome');
-});
+}); */
 
 Route::resource('orders', OrderController::class);
 
 // Rimorsoft Online - Dos opciones
-Route::view('/users', 'users.users')->name('users');
+Route::view('/', 'users.users')->name('users');
 
 //https://www.nicesnippets.com/blog/laravel-livewire-crud-with-bootstrap-modal-example
 //Route::get('users', Users::class);
