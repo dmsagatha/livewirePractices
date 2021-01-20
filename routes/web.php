0ebee885ed1e5769ae8788bdc846a5e11fa6b89c
Doris Morales <dmorales55@gmail.com>
2021-01-19 22:34:01 -0500

    CRUD de usuarios con Modal y buscador

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('orders', OrderController::class);
 
-// Rimorsoft Online - Dos opciones
+// Rimorsoft Online
 Route::view('/', 'users.users')->name('users');
 
-//https://www.nicesnippets.com/blog/laravel-livewire-crud-with-bootstrap-modal-example
-// https://www.youtube.com/watch?v=nnK1PdtAX_M
+// https://www.nicesnippets.com/blog/laravel-livewire-crud-with-bootstrap-modal-example
+// Surfside Media - https://www.youtube.com/watch?v=nnK1PdtAX_M
 Route::view('/users-modal', 'users-modal.users')->name('usersModal');
 
 // https://laracasts.com/series/guest-spotlight/episodes/3
 // Clovo - https://www.youtube.com/watch?v=CBL17MxWi_4
 Route::view('/users-table', 'users-table.users')->name('usersTable');
-
-// https://dev.to/dariusdauskurdis/laravel-8-crud-basic-steps-livewire-and-tailwind-10b
-Route::view('/companies', 'companies-modal.companies')->name('companiesModal');
